4469afca75b26753b9621be506767c3f2e715aff
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-31 09:11:41 -0400

    Section 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create(
+  content: "This is awesome content",
+  is_published: true
+)
+
+Post.create(
+  content: "This is better content",
+  is_published: false
+)
